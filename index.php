ed223582a7bbc67b98ed45103aff82dfdd0d954d
Alejandro Jiménez <94633477+Alejandro-Jimenez-Daza@users.noreply.github.com>
2024-04-11 10:11:04 -0500

    Update index.php
    title update!

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ curl_close($ch);
     <meta charset="UTF-8">
     <meta name="description" content="La próxima película de marvel">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Título de tu página</title>
+    <title>Próxima película de marvel</title>
     <link 
     rel="stylesheet" 
     href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css"/>
